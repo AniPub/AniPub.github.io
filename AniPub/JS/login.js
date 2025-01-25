1f4d4f8cc6b72c1011a5a1e3b0cd5ca82829804a
Adnan <103826456+AdnanDLuffy@users.noreply.github.com>
2025-01-25 14:00:47 -0500

    Add files via upload

---
+++ AniPub/JS/login.js
@@ -0,0 +1,16 @@
+let User  = document.querySelector(".username")
+let Pass = document.querySelector(".pass")
+
+function login() {
+    
+userCredential.forEach((value)=>{
+    if (value.email === User.value && value.passward === Pass.value) {
+        document.querySelector(".login-a").setAttribute("href","/Website/Home.html")
+    
+    }
+    else {
+        document.querySelector(".warning").innerText = `Please provide correct Info`
+    }
+
+})
+}
