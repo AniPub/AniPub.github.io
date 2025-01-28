7b24a46d075ea99bbf522ffc60cb372a9777d51e
Adnan <103826456+AdnanDLuffy@users.noreply.github.com>
2025-01-28 15:02:34 -0500

    Update data.js

--- AniPub/JS/data/data.js
+++ AniPub/JS/data/data.js
@@ -1,5 +1,4 @@
 
-
 export let OP = [
       [  {
             Anime: "One Piece",
@@ -9,7 +8,7 @@ export let OP = [
             Name:"One Piece",
             Synonyms: "OP",
             name:"Trailer",
-            link:"Video/One Piece.mp4",
+            link:"Video/One piece.mp4",
             title:"One piece | The Greatest story Ever Told Trailer",
             poster:"Poster Pic/One-Piece.jpg",
             Aired: "Oct 20,1999 to ?",
@@ -23,37 +22,37 @@ export let OP = [
             DescripTion:"One Piece is an iconic anime that follows the journey of Monkey D. Luffy, a young pirate with the extraordinary ability to stretch his body like rubber after consuming a Devil Fruit. Driven by his dream to become the Pirate King, Luffy sets sail to find the legendary treasure known as \"One Piece,\" said to be hidden at the end of the Grand Line. As Luffy assembles his diverse crew, the Straw Hat Pirates, they encounter a world filled with rich lore, complex characters, and deep mysteries. Each crew member has their own aspirations, from becoming the world's greatest swordsman to finding a long-lost friend, adding layers of depth to their adventures. The series explores themes of friendship, freedom, and the pursuit of dreams, while delving into the intricate history of the world, including the enigmatic Void Century and the powerful Ancient Weapons. Throughout their journey, the Straw Hat Pirates face formidable foes, including rival pirate crews, the oppressive World Government, and the mysterious Marines. Each battle reveals more about the world’s secrets and the true nature of the One Piece treasure itself. With its masterful storytelling, emotional depth, and a blend of humor and action, One Piece invites viewers to join Luffy and his crew on an unforgettable adventure filled with twists, revelations, and the enduring spirit of adventure.",
         },
         {
-            id : 1,
+        
             name:"Video1",
             link:"Video/one-piece-amv-welcome-to-onigashima-1080-publer.io.mp4",
             title:"One Piece amv welcome to Onigashima",
         },
         {
-            id : 2,
+            
             name:"Video2",
             link:"Video/Doflamingo ASMV - Justice Will Prevail - One Piece.mp4",
             title:"Doflamingo ASMV - Justice Will Prevail",
         },
         {   
-            id : 3,
+            
             name:"Video3",
             link:"Video/ ONE PIECE - UNLEASHED  Final Saga Trailer AMV.mp4",
             title:"One Piece - Unleashed Final Saga",
         },
         {   
-            id : 4,
+           
             name:"Video4",
             link:"Video/Scissor Seven.mp4",
             title:"Scissor Seven AMV",
         },
         {   
-            id : 5,
+            
             name:"Video5",
             link:"Video/Closer-Anime-Amv.mp4",
             title:"Closer Anime AMV",
         },
         {   
-            id : 6,
+           
             name:"Video6",
             link:"Video/Shadow Garden Amv.mp4",
             title:"Shadow Garden",
@@ -65,9 +64,10 @@ export let OP = [
     {
         id : 1,
         Name: "Naruto Shippuden",
+        name:"Trailer",
         ImagePath: "Poster Pic/Naruto-Shippudin.jpg",
-        Synonyms: "Naruto Hurricane Chronicles",
-            name:"Trailer",
+        
+            Synonyms: "Naruto Hurricane Chronicles",    
             link:"Video/Naruto/Naruto.mp4",
             title:"Naruto Blue Bird Intro ! Bangla Cover",
             poster:"Poster Pic/Naruto-Shippudin.jpg",
@@ -78,7 +78,7 @@ export let OP = [
             MALScore: "8.15",
             Genres:"Action ,Adventure ,Comedy ,Martial Arts ,Shounen ,Super Power",
             Studios: "Studio Pierrot",
-            Producers: "Producers: TV Tokyo, Aniplex, KSS, Rakuonsha, TV Tokyo Music, Shueisha, Studio Pierrot, Viz Media",
+            Producers: "TV Tokyo, Aniplex, KSS, Rakuonsha, TV Tokyo Music, Shueisha, Studio Pierrot, Viz Media",
             DescripTion:`Naruto Shippuden" picks up two and a half years after the original "Naruto" series, focusing on Naruto Uzumaki's return to the Hidden Leaf Village after intense training with Jiraiya. The series follows Naruto as he strives to rescue his friend Sasuke Uchiha, who has left the village to gain power from Orochimaru, a rogue ninja with sinister intentions.
 
             As the story unfolds, Naruto and his friends face the growing threat of the Akatsuki, a powerful organization of rogue ninjas seeking to capture the Tailed Beasts, including the Nine-Tails sealed within Naruto. Each member of the Akatsuki has their own motives and backstories, adding depth to the conflict. The series explores the complexities of their characters, revealing their pasts and the reasons behind their actions.
@@ -99,54 +99,152 @@ export let OP = [
 
 
     [
-    {
+    {   
+        name:"Trailer",
         id : 2,
         Name: "Attack On Titan",
         ImagePath: "Poster Pic/AttackOnTitan.jpg",
+        Synonyms: " Shingeki no Kyojin Season 4, Attack on Titan Season 4",    
+            link:"Video/Attack On Titan/My-Soldier-Rage.mp4",
+            title:"My Soldier Rage",
+            poster:"Poster Pic/AttackOnTitan.jpg",
+            Aired: "Jan 10, 2022 to Apr 4, 2022",
+            Premiered: "Winter 2022",
+            Duration: "24m",
+            Status: "Finished Airing",
+            MALScore: "8.97",
+            Genres:" Action Mystery Drama Fantasy Shounen Super Power Military",
+            Studios: "MPPA",
+            Producers: " Production I.G, Dentsu, Mainichi Broadcasting System, Pony Canyon, Kodansha, Pony Canyon Enterprise",
+            DescripTion:`"Attack on Titan" is a Japanese manga and anime series created by Hajime Isayama. Set in a world where humanity is on the brink of extinction due to giant humanoid creatures known as Titans, the story follows Eren Yeager, his adoptive sister Mikasa Ackerman, and their friend Armin Arlert. After a Titan breach leads to the destruction of their hometown and the death of Eren's mother, the trio joins the military to fight against the Titans and uncover the truth behind their existence. The series explores themes of survival, freedom, and the moral complexities of war, featuring intense action, political intrigue, and deep character development.
+            `,
+    
     },
+
+
 ],
 
 
     [
         {
+        name:"Trailer",    
         id : 3,
         Name: "Naruto: Shonen Jump",
         ImagePath: "Poster Pic/NarutoSJ.jpg",
-        },
+        Synonyms: " NARUTO Shounen Jump , ナルト ",    
+            link:"Video/Naruto ShounenJump/Trailer.mp4",
+            title:"Naruto Shounen Jump Trailer",
+            poster:"Poster Pic/NarutoSJ.jpg",
+            Aired: " Oct 3, 2002 to Feb 8, 2007",
+            Premiered: " Oct 3, 2002 to Feb 8, 2007",
+            Duration: "23m",
+            Status: "Finished Airing",
+            MALScore: "7.89",
+            Genres:"Action Adventure Comedy Martial Arts Shounen Super Power",
+            Studios: "Studio Pierrot",
+            Producers: "TV Tokyo, Aniplex, Shueisha, Studio Pierrot, Viz Media",
+            DescripTion:`Moments prior to Naruto Uzumaki's birth, a huge demon known as the Kyuubi, the Nine-Tailed Fox, attacked Konohagakure, the Hidden Leaf Village, and wreaked havoc. In order to put an end to the Kyuubi's rampage, the leader of the village, the Fourth Hokage, sacrificed his life and sealed the monstrous beast inside the newborn Naruto. Now, Naruto is a hyperactive and knuckle-headed ninja still living in Konohagakure. Shunned because of the Kyuubi inside him, Naruto struggles to find his place in the village, while his burning desire to become the Hokage of Konohagakure leads him not only to some great new friends, but also some deadly foes. [Written by MAL Rewrite]
+            `,
+        
+    
+    },
 ],
 
 
     [ 
           {
+        name:"Trailer",    
         id : 4,
         Name: "Boruto",
         ImagePath: "Poster Pic/Boruto.jpg",
-          },
-],
-
-
-    [   {
-        id : 5 ,
-        Name: "Seraph of the End",
-        ImagePath: "Poster Pic/SeraphOfTheEnd.jpg",
+        Synonyms: "Boruto: Naruto Next Generations",    
+            link:"Video/Boruto/Boruto.mp4",
+            title:"Boruto - Trailer",
+            poster:"Poster Pic/Boruto.jpg",
+            Aired: "Apr 5, 2017 to Mar 26, 2023",
+            Premiered: " Spring 2017",
+            Duration: "23m",
+            Status: "Finished Airing",
+            MALScore: "6.6",
+            Genres:" Action ,Adventure ,Martial, Arts ,Shounen ,Super Power",
+            Studios: "Studio Pierrot",
+            Producers: " TV Tokyo, Aniplex, Rakuonsha, Shueisha, Studio Pierrot, Viz Media",
+            DescripTion:`Following the successful end of the Fourth Shinobi World War, Konohagakure has been enjoying a period of peace, prosperity, and extraordinary technological advancement. This is all due to the efforts of the Allied Shinobi Forces and the village's Seventh Hokage, Naruto Uzumaki. Now resembling a modern metropolis, Konohagakure has changed, particularly the life of a shinobi. Under the watchful eye of Naruto and his old comrades, a new generation of shinobi has stepped up to learn the ways of the ninja. Boruto Uzumaki is often the center of attention as the son of the Seventh Hokage. Despite having inherited Naruto's boisterous and stubborn demeanor, Boruto is considered a prodigy and is able to unleash his potential with the help of supportive friends and family. Unfortunately, this has only worsened his arrogance and his desire to surpass Naruto which, along with his father's busy lifestyle, has strained their relationship. However, a sinister force brewing within the village may threaten Boruto's carefree life. New friends and familiar faces join Boruto as a new story begins in Boruto: Naruto Next Generations.
+            `,
+        
     },
 ],
 
 
     [   {
+        name:"Trailer",
+        id : 5 ,
+        Name: "Seraph of the End",
+        ImagePath: "Poster Pic/SeraphOfTheEnd.jpg",
+        Synonyms: "Seraph of the End , 終わりのセラフ",    
+            link:"Video/Seraph Of The End/Seraph Of The End.mp4",
+            title:"Seraph Of The End Billionera Song",
+            poster:"Poster Pic/SeraphOfTheEnd.jpg",
+            Aired: "Apr 4, 2015 to Jun 20, 2015",
+            Premiered: "Spring 2015",
+            Duration: "23m",
+            Status: "Finished Airing",
+            MALScore: "7.51",
+            Genres:" Action ,Drama ,Shounen, Vampire ,Supernatural ,Military",
+            Studios: "Wit Studio",
+            Producers: " Rakuonsha, NBCUniversal Entertainment Japan, Shueisha, Wit Studio, Funimation",
+            DescripTion:`With the appearance of a mysterious virus that kills everyone above the age of 13, mankind becomes enslaved by previously hidden, power-hungry vampires who emerge in order to subjugate society with the promise of protecting the survivors, in exchange for donations of their blood. Among these survivors are Yuuichirou and Mikaela Hyakuya, two young boys who are taken captive from an orphanage, along with other children whom they consider family. Discontent with being treated like livestock under the vampires' cruel reign, Mikaela hatches a rebellious escape plan that is ultimately doomed to fail. The only survivor to come out on the other side is Yuuichirou, who is found by the Moon Demon Company, a military unit dedicated to exterminating the vampires in Japan. Many years later, now a member of the Japanese Imperial Demon Army, Yuuichirou is determined to take revenge on the creatures that slaughtered his family, but at what cost? Owari no Seraph is a post-apocalyptic supernatural shounen anime that follows a young man's search for retribution, all the while battling for friendship and loyalty against seemingly impossible odds. [Written by MAL Rewrite] `,
+    
+    },
+],
+
+
+    [   {
+        name:"Trailer",
         id : 6,    
         Name: "Date A Live",
         ImagePath: "Poster Pic/DateALive.jpg",
+        Synonyms: "Date A Live , デート・ア・ライブ",    
+            link:"Video/Date a Live/Trailer.mp4",
+            title:"Data A Live Kururmi Amv",
+            poster:"Poster Pic/DateALive.jpg",
+            Aired: "Apr 6, 2013 to Jun 22, 2013",
+            Premiered: " Spring 2013 ",
+            Duration: "23m",
+            Status: "Finished Airing",
+            MALScore: "7.26",
+            Genres:"Comedy, Mecha ,Romance ,School ,Sci-Fi ,Harem",
+            Studios: "AIC Plus+",
+            Producers: " Kadokawa Shoten, AT-X, Nippon Columbia, Fujimi Shobo, AIC Plus+, Funimation",
+            DescripTion:`Thirty years ago, the Eurasian continent was devastated by a supermassive "spatial quake"—a phenomenon involving space vibrations of unknown origin—resulting in the deaths of over 150 million people. Since then, these quakes have been plaguing the world intermittently, albeit on a lighter scale. Shidou Itsuka is a seemingly average high school student who lives with his younger sister, Kotori. When an imminent spatial quake threatens the safety of Tengu City, he rushes to save her, only to be caught in the resulting eruption. He discovers a mysterious girl at its source, who is revealed to be a "Spirit," an otherworldly entity whose appearance triggers a spatial quake. Soon after, he becomes embroiled in a skirmish between the girl and the Anti-Spirit Team, a ruthless strike force with the goal of annihilating Spirits. However, there is a third party that believes in saving the spirits: "Ratatoskr," which surprisingly is commanded by Shidou's little sister! Kotori forcibly recruits Shidou after the clash, presenting to him an alternative method of dealing with the danger posed by the Spirits—make them fall in love with him. Now, the fate of the world rests on his dating prowess, as he seeks out Spirits in order to charm them. [Written by MAL Rewrite] `,
+    
     },
 ],
 
 
     [ 
         {   
+        name:"Trailer",    
         id : 7,
         Name:"Demon Slayer",
         ImagePath: "Poster Pic/DemonSlayer.jpg",
-        },
+        Synonyms: "鬼滅の刃 , Demon Slayer , Blade of Demon Destruction",    
+        link:"Video/Demon Slayer/LoveHashira.mp4",
+        title:"Love Hashira AMV",
+        poster:"Poster Pic/DemonSlayer.jpg",
+        Aired: "Apr 6, 2019 to Sep 28, 2019",
+        Premiered: "Spring 2019",
+        Duration: "23m",
+        Status: "Finished Airing",
+        MALScore: "8.57",
+        Genres:"Action,Demons,Historical, Shounen,Supernatural",
+        Studios: "ufotable",
+        Producers: " Aniplex, Shueisha, ufotable, Aniplex of America",
+        DescripTion:`Ever since the death of his father, the burden of supporting the family has fallen upon Tanjirou Kamado's shoulders. Though living impoverished on a remote mountain, the Kamado family are able to enjoy a relatively peaceful and happy life. One day, Tanjirou decides to go down to the local village to make a little money selling charcoal. On his way back, night falls, forcing Tanjirou to take shelter in the house of a strange man, who warns him of the existence of flesh-eating demons that lurk in the woods at night. When he finally arrives back home the next day, he is met with a horrifying sight—his whole family has been slaughtered. Worse still, the sole survivor is his sister Nezuko, who has been turned into a bloodthirsty demon. Consumed by rage and hatred, Tanjirou swears to avenge his family and stay by his only remaining sibling. Alongside the mysterious group calling themselves the Demon Slayer Corps, Tanjirou will do whatever it takes to slay the demons and protect the remnants of his beloved sister's humanity. [Written by MAL Rewrite] `,
+
+    
+    },
+   
 ]
 
 
