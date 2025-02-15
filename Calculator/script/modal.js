4403d95f17f900b4041b9db914b9bb7855857372
Adnan <103826456+AdnanDLuffy@users.noreply.github.com>
2025-02-15 10:39:39 -0500

    Delete Calculator directory

--- Calculator/script/modal.js
+++
@@ -1,9 +0,0 @@
-const devsMessage = document.querySelector("dialog");
-document.querySelector(".am-b").addEventListener('click', ()=> {
-devsMessage.showModal();
-
-})
-document.querySelector(".leave").addEventListener('click', ()=> {
-    devsMessage.close();
-    
-    })
